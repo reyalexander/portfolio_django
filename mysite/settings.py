22029abe7e227d96bdefcd4dd6f78e4d07d61e81
reyalexander <alexandercayromamani@gmail.com>
2023-03-10 12:58:12 -0500

    render deployment settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     }
 }
 
-'''
+
 
 DATABASES = {
     'default': {
@@ -112,8 +112,13 @@ DATABASES = {
         'PASSWORD': 'root',
     }
 }
-
-
+'''
+DATABASES = {
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3", 
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
