e98829758aeacdfe256a5fe74f7b54dc009b205f
reyalexander <alexandercayromamani@gmail.com>
2023-03-10 11:01:23 -0500

    UPDATE: modifed Settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Quick-start development settings - unsuitable for production
